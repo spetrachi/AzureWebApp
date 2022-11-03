11286188422f3b896bb381280ef851ee0b137863
Simone Petrachi <simone.petrachi@exprivia.com>
2022-11-03 21:18:56 +0100

    initial commit

---
+++ src/main/java/it/learning/AzureWebApp/BaseController.java
@@ -0,0 +1,14 @@
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BaseController {
+
+    @RequestMapping("/")
+        public String showHome((Model model) {
+            model.addAttribute("user", user);
+            return "home.html";
+        }
+}
---
+++ src/test/java/it/learning/AzureWebApp/AzureWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package it.learning.AzureWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
